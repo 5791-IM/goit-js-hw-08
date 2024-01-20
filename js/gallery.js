ff85fc48fb6a6011faa9defa1b462134b74c0648
5791-IM <142788119+5791-IM@users.noreply.github.com>
2024-01-20 22:12:14 +0200

    .

--- js/gallery.js
+++ js/gallery.js
@@ -63,3 +63,31 @@ const images = [
     description: "Lighthouse Coast Sea",
   },
 ];
+
+const galleryList = document.querySelector(".gallery");
+
+const markup = images.map(({ preview, original, description }) => {
+  return `<li class="gallery-item">
+    <a class="gallery-link"
+    href="${original}">
+      <img
+       class="gallery-image"
+        src="${preview}"
+         data-source="${original}"
+          alt="${description}"/>
+    </a>
+  </li>`;
+});
+
+galleryList.insertAdjacentHTML("beforeend", markup.join(""));
+
+galleryList.addEventListener("click", handleImg);
+
+let instance;
+
+function handleImg(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery-image")) {
+    return;
+  }
+}
